94421dcb2bb6773dec32554b548a21940e7fc975
Marcela Alvarez <marcealv@c2r7s2.42urduliz.com>
2023-03-07 16:40:39 +0100

    ex000 a 02

---
+++ c04/ex00/ft_strlen.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcealv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/24 11:07:11 by marcealv          #+#    #+#             */
+/*   Updated: 2023/03/07 15:51:12 by marcealv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	length;
+
+	length = 0;
+	while (*str != '\0')
+	{
+		length++;
+		str++;
+	}
+	return (length);
+}
+/*
+int	main(void)
+{
+	char	*s;
+
+	s = "marcealv";
+	printf("%d\n", ft_strlen(s));
+}
+*/
---
+++ c04/ex02/ft_putnbr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcealv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/22 11:19:56 by marcealv          #+#    #+#             */
+/*   Updated: 2023/03/07 15:40:30 by marcealv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	if (nb < 10)
+	{
+		ft_putchar(nb + '0');
+	}
+}
+
+/* int	main(void)
+{
+	ft_putnbr(10);
+	return (0);
+} */
